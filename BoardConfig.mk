3a97b9925e2680edadc8f9b6a8e7284c2f79ad98
Mark <void+github@voidzero.net>
2015-09-12 23:48:12 +0200

    Updated BoardConfig and added dt.img.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,31 +1,72 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Model Ids
+# 0PKV10000 - HTC Europe
+# msm8939 board, msm8916 kernel
+
+# Thank you CaptainThrowback for giving me something to base this on
+# https://github.com/CaptainThrowback/android_device_htc_m8ql
+
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 -include vendor/htc/m8qlul/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8939
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8939
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+
+# Added by mkvendor.sh
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := m8qlul
-
+# Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_BASE := 0x80078000
 BOARD_KERNEL_PAGESIZE := 2048
-
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
-
 TARGET_PREBUILT_KERNEL := device/htc/m8qlul/kernel
 
+# Added by voidzero
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01f88000 --dt device/htc/m8qlul/recovery/dt.img
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x02000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 02000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x100000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x268000000
+
+# The device only has volume and power buttons.
 BOARD_HAS_NO_SELECT_BUTTON := true
+
