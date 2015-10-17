8ea9df075dc955fd612dd12db71b4df21e63d6ed
Mark <void+github@voidzero.net>
2015-10-17 23:15:45 +0200

    Inherit gps_eu

--- device_m8qlul.mk
+++ device_m8qlul.mk
@@ -15,7 +15,7 @@
 #
 
 # The gps config appropriate for this device
-#$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
