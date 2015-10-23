dd41a3a104e164619f6e84fe69f4ae789a1c5460
Mark <void+github@voidzero.net>
2015-10-23 02:10:51 +0200

    Init: merged init.qcom.rc and init.target.rc
    Also added my imfamous vim folds. Needs further cleanup.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -94,14 +94,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.htc.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
