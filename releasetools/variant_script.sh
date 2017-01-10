c97251f50854a4075a92098a522710e51ef1da60
Mark van Dijk <void+github@voidzero.net>
2017-01-10 09:28:23 +0100

    Fix radio detection.

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -16,19 +16,22 @@ set -e
 
 # Create modem firmware links based on the currently installed modem
 mkdir -p /firmware/radio
-busybox mount -o shortname=lower -t vfat /dev/block/bootdevice/by-name/radio /firmware/radio
 
-base="/firmware/radio/mba"
+busybox mount /dev/block/bootdevice/by-name/radio /firmware/radio
+#busybox mount /system
 
-ln -s $base.mdt /system/vendor/firmware/mba.mdt
-ln -s $base.b00 /system/vendor/firmware/mba.b00
-ln -s $base.b01 /system/vendor/firmware/mba.b01
-ln -s $base.b02 /system/vendor/firmware/mba.b02
-ln -s $base.b03 /system/vendor/firmware/mba.b03
+base="/firmware/radio/MBA"
+
+ln -s $base.MDT /system/vendor/firmware/mba.mdt
+ln -s $base.B00 /system/vendor/firmware/mba.b00
+ln -s $base.B01 /system/vendor/firmware/mba.b01
+ln -s $base.B02 /system/vendor/firmware/mba.b02
+ln -s $base.B03 /system/vendor/firmware/mba.b03
 
 if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then
   exit 1
 fi
 
 busybox umount /firmware/radio
+#busybox umount /system
 exit 0
