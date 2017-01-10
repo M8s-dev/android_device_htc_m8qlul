d9e77b5d9d78a7bd69598a9cae16c1338f14731b
Mark van Dijk <void+github@voidzero.net>
2017-01-10 11:17:03 +0100

    Add libshim camera

---
+++ libshim_camera/libshim_camera.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#define LOG_TAG "HW_CAM_SHIM"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <utils/Log.h>
+#include <hardware/power.h>
+#include <hardware/hardware.h>
+
+#ifndef __unused
+#define __unused  __attribute__((__unused__))
+#endif
+
+ void __ZTVN7android8hardware10CameraInfoE () {
+ }
+
+void _ZN7android8hardware7ICamera11asInterfaceERKNS_2spINS_7IBinderEEE () {
+}
+
+void _ZN7android6Camera6createERKNS_2spINS_8hardware7ICameraEEE () {
+}
+
+void _ZN7android21ICameraRecordingProxy20getCommonBaseAddressEv () {
+}
