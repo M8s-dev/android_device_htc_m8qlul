d3781eb3d88ae020441e47faaaf64b418a6166ed
Mark van Dijk <void+github@voidzero.net>
2017-04-30 09:16:50 +0200

    Add CameraWrapper. * With this commit we: 1) fix camera crashing when switching from front to back camera by disabling face detection 2) hardcode number of cameras to 2. Third camera causes third-party apps to crash i.e snapchat 3) TODO! maybe we don't need rotation mismatch lines. I'll test the camera and see.

--- camera/Android.mk
+++ camera/Android.mk
@@ -13,6 +13,22 @@
 # limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.msm8916
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
