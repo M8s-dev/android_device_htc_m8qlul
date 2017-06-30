d54f543e487b386aface54b076d84c8906473290
Mark van Dijk <void+github@voidzero.net>
2017-06-30 09:29:52 +0200

    Sensors: Fix sensors by fixing the copying of init.sensors.rc file.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -55,9 +55,10 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE	   := init.sensors.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_TAGS  := ETC
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES	   := etc/init.sensors.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.htc.power.rc
