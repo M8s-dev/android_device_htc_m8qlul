7385ffa21d4f68f6079478ba048a25d9e6517d5d
Mark van Dijk <void+github@voidzero.net>
2017-07-21 17:16:09 +0200

    Camera shim: Add missing symbols There's seems to be many missing that's bad and weird. maybe something's missing here or I need newer libs

--- libshim_camera/libshim_camera.c
+++ libshim_camera/libshim_camera.c
@@ -27,8 +27,8 @@
 #define __unused  __attribute__((__unused__))
 #endif
 
- void __ZTVN7android8hardware10CameraInfoE () {
- }
+void _ZTVN7android8hardware10CameraInfoE () {
+}
 
 void _ZN7android8hardware7ICamera11asInterfaceERKNS_2spINS_7IBinderEEE () {
 }
@@ -36,5 +36,203 @@ void _ZN7android8hardware7ICamera11asInterfaceERKNS_2spINS_7IBinderEEE () {
 void _ZN7android6Camera6createERKNS_2spINS_8hardware7ICameraEEE () {
 }
 
+void _ZN7android8hardware14ICameraService10descriptorE () {
+}
+
+void _ZN7android6Camera7connectEiRKNS_8String16Eii () {
+}
+
 void _ZN7android21ICameraRecordingProxy20getCommonBaseAddressEv () {
+}
+
+void _ZN7android8hardware7ICameraD0Ev () {
+}
+
+void _ZN7android8hardware7camera26params19VendorTagDescriptorC2Ev () {
+}
+
+void _ZN7android8hardware14ICameraServiceD0Ev () {
+}
+
+void _ZN7android8hardware14ICameraServiceD1Ev () {
+}
+
+void _ZN7android8hardware7camera26params19VendorTagDescriptorD2Ev () {
+}
+
+void _ZN7android6Camera18setVideoBufferModeEi () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor13writeToParcelEPNS_6ParcelE () {
+}
+
+void _ZN7android8hardware7camera26params19VendorTagDescriptor14readFromParcelEPKNS_6ParcelE () {
+}
+
+void _ZN7android6Camera14setVideoTargetERKNS_2spINS_22IGraphicBufferProducerEEE () {
+}
+
+void _ZN7android8hardware15BnCameraService10onTransactEjRKNS_6ParcelEPS2_j () {
+}
+
+void _ZN7android6Camera27releaseRecordingFrameHandleEP13native_handle () {
+}
+
+void _ZN7android10CameraBaseINS_6CameraENS_12CameraTraitsIS1_EEE13getCameraInfoEiPNS_8hardware10CameraInfoE () {
+}
+
+void _ZN7android8hardware7ICamera10descriptorE () {
+}
+
+void _ZN7android8hardware14ICameraService11asInterfaceERKNS_2spINS_7IBinderEEE () {
+}
+
+void _ZN7android8hardware7ICameraD1Ev () {
+}
+
+void _ZN7android14CameraMetadata14getTagFromNameEPKcPKNS_19VendorTagDescriptorEPj () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor11getTagCountEv () {
+}
+
+void _ZN7android8hardware8BnCamera10onTransactEjRKNS_6ParcelEPS2_j () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor11getTagArrayEPj () {
+}
+
+void _ZNK7android8hardware14ICameraService22getInterfaceDescriptorEv () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor14getSectionNameEj () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor10getTagNameEj () {
+}
+
+void _ZNK7android8hardware7ICamera22getInterfaceDescriptorEv () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor10getTagTypeEj () {
+}
+
+void _ZTTN7android8hardware15BnCameraServiceE () {
+}
+
+void _ZTTN7android8hardware8BnCameraE () {
+}
+
+void _ZTVN7android8hardware7camera24impl19CaptureResultExtrasE () {
+}
+
+void _ZThn4_N7android8hardware15BnCameraService10onTransactEjRKNS_6ParcelEPS2_j () {
+}
+
+void _ZThn4_N7android8hardware8BnCamera10onTransactEjRKNS_6ParcelEPS2_j () {
+}
+
+void _ZN7android8hardware7camera218BnCameraDeviceUser10onTransactEjRKNS_6ParcelEPS3_j () {
+}
+
+void _ZTv0_n12_N7android8hardware14ICameraServiceD0Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware14ICameraServiceD1Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware14ICameraServiceD2Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware7ICameraD0Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware7ICameraD1Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware7ICameraD2Ev () {
+}
+
+void _ZN7android8hardware7camera217ICameraDeviceUser10descriptorE () {
+}
+
+void _ZN7android8hardware7camera217ICameraDeviceUserD0Ev () {
+}
+
+void _ZN7android8hardware7camera217ICameraDeviceUserD1Ev () {
+}
+
+void _ZN7android8hardware7camera217ICameraDeviceUserD2Ev () {
+}
+
+void _ZNK7android8hardware7camera217ICameraDeviceUser22getInterfaceDescriptorEv () {
+}
+
+void _ZTTN7android8hardware7camera218BnCameraDeviceUserE () {
+}
+
+void _ZThn4_N7android8hardware7camera218BnCameraDeviceUser10onTransactEjRKNS_6ParcelEPS3_j () {
+}
+
+void _ZTv0_n12_N7android8hardware7camera217ICameraDeviceUserD0Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware7camera217ICameraDeviceUserD1Ev () {
+}
+
+void _ZTv0_n12_N7android8hardware7camera217ICameraDeviceUserD2Ev () {
+}
+
+void _ZTVN7android8hardware7camera214CaptureRequestE () {
+}
+
+void _ZN7android8hardware14ICameraServiceC2Ev () {
+}
+
+void _ZN7android8hardware14ICameraServiceD2Ev () {
+}
+
+void _ZN7android8hardware7camera26params19VendorTagDescriptoraSERKS3_ () {
+}
+
+void _ZN7android8hardware7ICameraC2Ev () {
+}
+
+void _ZN7android8hardware7ICameraD2Ev () {
+}
+
+void _ZNK7android8hardware7camera26params19VendorTagDescriptor4dumpEiii () {
+}
+
+void _ZN7android8hardware7camera217ICameraDeviceUserC2Ev () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration24getGraphicBufferProducerEv () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration14getSurfaceTypeEv () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration11getRotationEv () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration15getSurfaceSetIDEv () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration8getWidthEv () {
+}
+
+void _ZNK7android8hardware7camera26params19OutputConfiguration9getHeightEv () {
+}
+
+void _ZNSt3__113__vector_baseIN7android8hardware7camera214CaptureRequestENS_9allocatorIS4_EEED2Ev () {
+}
+
+void _ZNK7android14CameraMetadata6unlockEPK15camera_metadata () {
+}
+
+void _ZN7android14CameraMetadata6updateERK24camera_metadata_ro_entry () {
+}
+
+void _ZN7android13GraphicBufferC1Ejjij () {
 }
