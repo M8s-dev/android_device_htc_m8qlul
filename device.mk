ed09ed29be0a4e0a4e44a176241dddca76a60223
Julian Veit <claymore1298@gmail.com>
2018-01-13 10:05:34 +0100

    m8qlul: Remove audiod
    Conflicts:
    	hima.mk
    	rootdir/etc/init.qcom.rc

--- device.mk
+++ device.mk
@@ -80,7 +80,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio.r_submix.default \
