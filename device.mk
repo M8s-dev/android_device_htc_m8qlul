414293615e145daf0d68e9f9e1856f69fad29a46
Mark (voidzero) <void+github@voidzero.net>
2018-05-01 12:11:43 +0200

    Remove GPS HAL for now.

--- device.mk
+++ device.mk
@@ -172,7 +172,7 @@ PRODUCT_PACKAGES += \
     org.simalliance.openmobileapi.xml
 
 # GPS
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
