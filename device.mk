5977e0f6d71814b02588bfad222b6f24ac685e4e
Mark (voidzero) <void+github@voidzero.net>
2018-05-01 14:54:01 +0200

    Enable compilation of android.hidl.manager@1.0-java

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0-java
+
 # HTC Log Symbols
 PRODUCT_PACKAGES += \
     liblog_shim
