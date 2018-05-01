a91569ac4b311c58bd62bd35f57b5ab3fbd036b8
Mark (voidzero) <void+github@voidzero.net>
2018-05-01 14:25:47 +0200

    Update display HALs for O.

--- device.mk
+++ device.mk
@@ -138,6 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8916 \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
