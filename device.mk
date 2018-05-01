ded304dc1c269dffe4b8f4e143c6404857156ac8
Mark (voidzero) <void+github@voidzero.net>
2018-05-01 14:24:13 +0200

    Update audio for O.

--- device.mk
+++ device.mk
@@ -78,6 +78,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8916 \
