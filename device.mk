a8dcb46be2452a01bbe6cf2434615f60f22fc3cb
Mark (voidzero) <void+github@voidzero.net>
2018-05-01 14:49:37 +0200

    Build Vibrator HAL.

--- device.mk
+++ device.mk
@@ -352,6 +352,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
