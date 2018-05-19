2a9ba9a6debe7efc056a40c739c6247ac3ef57cc
root-expert <20chris09@gmail.com>
2018-05-19 13:16:17 +0200

    Add GPS HAL. * LA.BR.1.2.9.1_rb1.5

--- device.mk
+++ device.mk
@@ -180,10 +180,8 @@ PRODUCT_PACKAGES += \
     org.simalliance.openmobileapi.xml
 
 # GPS
-#PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
