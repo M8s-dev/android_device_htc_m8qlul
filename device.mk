0e0195054a70cdefc1460a75a531b399c64cb40c
root-expert <20chris09@gmail.com>
2018-05-24 13:42:43 +0200

    Add DRM correctly.

--- device.mk
+++ device.mk
@@ -153,6 +153,11 @@ PRODUCT_PACKAGES += \
     libgenlock \
     liboverlay
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Filesystem
 PRODUCT_PACKAGES += \
     e2fsck \
