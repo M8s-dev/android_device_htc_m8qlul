a197b45f455d59841b19bd2f7d5ece6ee3f41463
root-expert <20chris09@gmail.com>
2018-05-27 10:41:35 +0200

    Why i commented out those?

--- device.mk
+++ device.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
-#$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-#$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Inherit the vendor files
 $(call inherit-product-if-exists, vendor/htc/m8qlul/m8qlul-vendor.mk)
@@ -337,7 +337,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
-    init.qcom.mem.sh
+    init.qcom.mem.sh \
     init.qcom.sh
 
 # Recovery
