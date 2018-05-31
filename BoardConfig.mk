231734523e6c6849ca8c670051972b4987fa59aa
root-expert <20chris09@gmail.com>
2018-05-31 11:40:41 +0200

    Remove extra.prop from BoardConfig as well.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,8 +187,7 @@ TARGET_POWERHAL_VARIANT := qcom
 
 # Properties    {{{
 TARGET_SYSTEM_PROP += \
-    $(DEVICE_PATH)/ril.prop \
-    $(DEVICE_PATH)/extra.prop
+    $(DEVICE_PATH)/ril.prop
 # }}}
 
 # Qualcomm support    {{{
