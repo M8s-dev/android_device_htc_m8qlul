ebb2ee3424229d6bc41d302eb9e237e292aea788
root-expert <20chris09@gmail.com>
2018-06-08 17:11:17 +0200

    m8qlul: camera: Build camera.device@1.0-impl-legacy

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     libmm-qcamera \
     libshim_camera \
     Snap \
