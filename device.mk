a61b93d2576c4c9858b769c1ee950d2ec56e80c4
Julian Veit <claymore1298@gmail.com>
2018-06-22 22:30:02 +0200

    m8qlul: switch to non-binderized gralloc
    This seems to solve the whatsapp-video playback and video call issue.
    
    Change-Id: Ib209e2625622eb414348867e0197ee67eceff960

--- device.mk
+++ device.mk
@@ -141,7 +141,6 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     copybit.msm8916 \
