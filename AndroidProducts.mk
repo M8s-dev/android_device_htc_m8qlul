f5efe3193c4ccb0a12419bfc0bee52f2170bab2e
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:21:40 +0100

    m8qlul: Consider lineage_m8qlul product via the AOSP way

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,12 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,4 +15,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_m8qlul.mk
+    $(LOCAL_DIR)/lineage_m8qlul.mk
